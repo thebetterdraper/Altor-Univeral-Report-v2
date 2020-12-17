0c7671646c24b78b30a4ab1d6b91e3cea1ce3cb3
Anirban DG <anirbandg1@gmail.com>
2020-12-17 20:07:26 +0530

    Added helper image

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -4,6 +4,7 @@ import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 
 
+
 // import downloadImage from './images/download.svg';
 
 
@@ -26,8 +27,14 @@ function Header(){
             {/* <button onClick={downloadPdf}><img src={downloadImage} alt="download" style={{width:'50px', height:'50px'}}></img></button> */}
             <h2 className="storeName">{store_name}</h2>
 
-            <Popup trigger={<button style={{width:"50px",height:"50px",borderRadius:"100%",backgroundColor:"white",border:"2px solid lightgray",color:"gray",fontFamily:"cursive"}}>i</button>} position="right center">
-                <div>Popup content here !!</div>
+            <Popup trigger={<button style={{width:"25px",height:"25px",borderRadius:"100%",backgroundColor:"white",border:"1px solid lightgray",color:"gray",fontStyle:"italic",fontFamily:"cursive",marginTop:"28px",marginLeft:"18%",cursor:"pointer"}}>i</button>} position="right center">
+                <div style={{width:"210px",height:"auto",backgroundColor:"grey"}}>
+                    <img style={{width:"200px",height:"auto",padding:"5px",borderRadius:"5px"}} src={require('./helperimage.png')}/>
+                    <br></br>
+                    <font style={{fontFamily:"sans-serif",fontSize:"20px",color:"lightgray"}}>The number in the centre represents the corresponding score for the rider today. The number at the bottom right represents how different it is in percentage terms from the same score of the same rider, on the first of the same month.</font>
+                    
+                    
+                </div>
             </Popup>
         </div>
     );
