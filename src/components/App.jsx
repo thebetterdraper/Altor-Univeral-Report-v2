74a2dfe712310ff0ebc6f300ad9a914dbe9593ed
Soumil Bose <samuelroy41@gmail.com>
2020-12-22 19:03:46 +0530

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import {Switch,Route} from "react-router-dom";
+
 import AltorRider from "./AltorRider";
-import Home from "./Home";
-import Navbar from "./Navbar";
+
 
 function App(){
 
     return (
         
-            
-                <AltorRider></AltorRider>
-              
+                <AltorRider></AltorRider>    
        
     );
 
